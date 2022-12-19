3b28acf298a565170d4ed5a9d3112453c011241c
wonyong.yun <wonyong.yun@playlinks.com>
2022-12-20 00:59:06 +0900

    [stone] Escape Sequence

---
+++ 1_stone/step1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Escape Sequence\n");
+	printf("\\n = New Line\n");
+	printf("\\r = Carriage Return\n");
+	printf("\\b = Back Space\n");
+	printf("\\t = Tab\n");
+	printf("\\a = alarm\n");
+	return 0;
+}
+
