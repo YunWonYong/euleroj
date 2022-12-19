276e709f65e1fe0979dbd90ea3c2997322f3a247
wonyong.yun <wonyong.yun@playlinks.com>
2022-12-20 01:39:53 +0900

    [stone] step1 exercise 3

---
+++ 1_stone/step1/exercise3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+	printf("EEEEEEE\tU     U\tL\tEEEEEEE\tRRRRRR\n");
+	printf("E\tU     U\tL\tE\tR     R\n");
+	printf("EEEEEEE\tU     U\tL\tEEEEEEE\tRRRRRRR\n");
+	printf("E\tU     U\tL\tE\tR    R\n");
+	printf("EEEEEEE\tUUUUUUU\tLLLLLLL\tEEEEEEE\tR     R\n");
+	return 0;
+}
