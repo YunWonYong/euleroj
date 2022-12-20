5cc88e409279be2759856e71c1d7bb50566936ef
wonyong.yun <wonyong.yun@playlinks.com>
2022-12-20 23:31:11 +0900

    [stone] step2 exercise 3

---
+++ 1_stone/step2/exercise3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("%5d\n", 22222);
+	printf("%5d\n", 2222);
+	printf("%5d\n", 222);
+	printf("%5d\n", 22);
+	printf("%5d\n", 2);
+	printf("-----\n");
+	printf("%5d\n", 22222 - 2222 - 222 - 22 - 2);
+	return 0;
+}
