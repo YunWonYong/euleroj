895b4e426a9565a27eba54a6bfc0ecf8f0eb3fe5
wonyong.yun <wonyong.yun@playlinks.com>
2022-12-20 23:55:39 +0900

    [stone] stop3 exercise 2

---
+++ 1_stone/step3/exercise2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("%10.3lf\n", 12.5672);
+	printf("%10.3lf\n", 456.7769);
+	printf("%10.3lf\n", 123456.78);
+	printf("%10.3lf\n", 4567.5678);
+	printf("%10.3lf\n", 6712.34523);
+	return 0;
+}
