63089f44bfa14aa7b2fa654080483ebb7f9ed20e
wonyong.yun <wonyong.yun@playlinks.com>
2022-12-22 23:16:02 +0900

    [stone] step4 exercise 2

---
+++ 1_stone/step4/exercise2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	double a = 12.34, b = 23.12;
+	printf("%.2lf + %.2lf = %.2lf\n", a, b, a + b);
+	printf("%.2lf - %.2lf = %.2lf\n", a, b, a - b);
+	printf("%.2lf * %.2lf = %.2lf\n", a, b, a * b);
+	printf("%.2lf / %.2lf = %.2lf\n", a, b, a / b);
+	return 0;
+}
