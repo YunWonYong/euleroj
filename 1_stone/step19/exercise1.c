f742057193d8e270f4ff9bf48f1caabadfe8e896
wonyong.yun <wonyong.yun@playlinks.com>
2023-01-29 12:22:42 +0900

    [stone] step19 exercise 1

---
+++ 1_stone/step19/exercise1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int i = 0, j = 0, range = 5;
+	while(i < range) {
+		++i;
+		while(j < i) {
+			printf("%d", ++j);
+		}
+		j = 0;
+		printf("\n");
+	}
+	return 0;
+}
